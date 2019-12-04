dba46b2c313967d63fc2a3e6b97f09cd762477b2
Crystal Soo Lee <soo.leesj@gmail.com>
2019-12-04 13:34:00 -0800

    render the img obj and gave key selector

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # lab-02
-jQuery Selectors and Events
+
+Number and name of feature: #1: Display images
+
+Estimate of time needed to complete: 1hr
+
+Start time: 9:00 AM
+
+Finish time: 10:00AM
+
+Actual time needed to complete: 1 hr
+
+***
+
+Number and name of feature: #2: Filter images
+
+Estimate of time needed to complete: 2hr
+
+Start time: 10:00 AM
+
+Finish time: 12:00 PM
+
+Actual time needed to complete: 2 hr
+
+***
+
+Number and name of feature: #3: Style the application
+
+Estimate of time needed to complete: 1hr
+
+Start time: 1:00 PM
+
+Finish time: 2:00 PM
+
+Actual time needed to complete: 1 hr
--- js/app.js
+++ js/app.js
@@ -1,19 +1,80 @@
-'use strict';
+'use strict'
 
-function HornImage(img) {
-  this.title = img.title;
-  this.image_url = img.image_url;
-  this.description = img.description;
-  this.description = img.keyword;
-  this.description = img.horns;
+function HornImage(obj) {
+  this.image_url = obj.image_url;
+  this.title = obj.title;
+  this.horns = obj.horns;
+  this.keyword = obj.keyword;
+  this.description = obj.description;
+
+  allImages.push(this);
 }
 
-HornImage.allImages = [];
+const allImages =[];
 
-HornImage.prototype.render = function() {
-  let template = Handlebars.compile($('#photo-template').html());
-  console.log('template :', template);
-  return template(this);
-};
+HornImage.prototype.renderHornObject = function() {
+  $('main').append('<section class="clone"><section>');
+  let $clone = $('section[class="clone"]');
+  let cloneTemplate = $('#photo-template').html();
+
+  $clone.html(cloneTemplate);
+
+  $clone.find('h2').text(this.title);
+  $clone.find('p').text(this.description);
+  $clone.find('img').attr('src', this.image_url);
+
+  $clone.removeClass('clone');
+  $clone.attr('class', this.keyword);
+}
+
+
+HornImage.prototype.selectByKeyword = function() {
+  let filterKeywords = [];
+  $('option').not(':first').remove();
+  allImages.forEach(image => {
+    if (!filterKeywords.includes(image.keyword)) {
+      filterKeywords.push(image.keyword);
+    }
+  });
+
+  filterKeywords.sort();
+
+  filterKeywords.forEach(keyword => {
+    let optionTag = `<option value="${keyword}">${keyword}</option>`;
+    $('select').append(optionTag);
+  });
+}
+
+function filterHornImg() {
+  $('select').on('change', function () {
+    let selectedKeyword = $(this).val();
+    if(selectedKeyword !== 'default') {
+      $('section').hide();
+      $(`section[class = "${selectedKeyword}"]`).show();
+    }
+  });
+}
+
+
+function readJsonData() {
+  $.get('./data/page-1.json', 'json')
+    .then(data => {
+      data.forEach(hornImglObj => {
+        new HornImage(hornImglObj);
+      })
+    })
+    .then(() => {
+      allImages.forEach( image =>{
+        image.renderHornObject();
+        image.selectByKeyword();
+      })
+    })
+}
+
+$(() => {
+  readJsonData();
+  filterHornImg();
+});
+
 
 
